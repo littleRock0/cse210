353f92029f1cf60287521d8d82a58acb5d277ddc
littleRock0 <irochelle@byupathway.edu>
2025-07-11 17:57:04 -0600

    excercize 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -7,8 +7,36 @@ using static System.Console;
 
 class Program
 {
+    const int RANGE_END = 101;
+    
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random number = new Random();
+        int myNumber = number.Next(1, RANGE_END);
+        
+        WriteLine("Welcome to \"Guess My Number\"\n");
+        WriteLine($"I have a number between 1 and {RANGE_END - 1}.");
+        
+        int guess;
+        
+        do 
+        {
+            Write("Your Guess (whole number): ");
+            
+            guess = int.Parse(ReadLine());
+            
+            if (guess == myNumber)
+            {
+                WriteLine($"Correct my number is: {myNumber}\n");
+            }
+            else if (guess > myNumber)
+            {
+                WriteLine($"Sorry {guess} is too large, guess again.\n");
+            }
+            else if (guess < myNumber)
+            {
+                WriteLine($"Sorry {guess} is too small guess again.\n");
+            }
+        } while (guess != myNumber);
     }
 }
