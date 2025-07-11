064a7154a0844aa3160623d4057b184fd4b184ba
littleRock0 <irochelle@byupathway.edu>
2025-07-10 22:59:56 -0600

    added full switch statement

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -10,19 +10,24 @@ class Program
     static void Main(string[] args)
     {
         Write("Grade (as a percentage not do NOT add ): ");
+        
         string input = ReadLine();
         int grade = int.Parse(input);
         
         string score = grade switch
         {
-            grade >= 93 => "A",
-            
-            grade <= 93 && grade >= 90 => "A-",
-                
-            grade > 90 && grade >= 87 => "B+",
-                
-            case int i when grade >= 93 && grade >= 90:
-                return
+            >= 93 => "A",
+            >= 90 => "A-",
+            >= 87 => "B+",
+            >= 83 => "B",
+            >= 80 => "B-",
+            >= 77 => "C+",
+            >= 73 => "C",
+            >= 70 => "C-",
+            >= 67 => "D+",
+            >= 63 => "D",
+            >= 60 => "D-",
+            < 60 => "F",
         };
     }
 }
