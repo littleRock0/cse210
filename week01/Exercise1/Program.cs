b97ae54c262e527923eb873b715e8fbb1af91faa
littleRock0 <irochelle@byupathway.edu>
2025-07-10 20:57:03 -0600

    excersize1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,9 +1,20 @@
+/******************************************************************************
+ * replaced WriteLine with Write to get rid of appended newline character
+ * added using static System.Console; to shorten the code
+ */
 using System;
+using static System.Console;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Write("First name: ");
+        string firstName = ReadLine();
+        
+        Write("Last Name: ");
+        string lastName = ReadLine();
+        
+        Write($"Hello {lastName}, {firstName} {lastName}");
     }
 }
