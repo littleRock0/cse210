2233fa981b095bee2d530ee1be5b1da020b5cfee
littleRock0 <irochelle@byupathway.edu>
2025-07-10 21:04:57 -0600

    added my standard start

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,4 +1,9 @@
+/*******************************************************************************
+ * replaced WriteLine with Write to get rid of appended newline character
+ * added using static System.Console; to shorten the code
+ ******************************************************************************/
 using System;
+using static System.Console;
 
 class Program
 {
