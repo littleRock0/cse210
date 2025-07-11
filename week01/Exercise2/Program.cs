ba0e7169486fba75934582adc2cf76f9a0f5742d
littleRock0 <irochelle@byupathway.edu>
2025-07-10 22:38:22 -0600

    working on excercize2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -9,6 +9,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Write("Grade (as a percentage not do NOT add ): ");
+        string input = ReadLine();
+        int grade = int.Parse(input);
+        
+        string score = grade switch
+        {
+            grade >= 93 => "A",
+            
+            grade <= 93 && grade >= 90 => "A-",
+                
+            grade > 90 && grade >= 87 => "B+",
+                
+            case int i when grade >= 93 && grade >= 90:
+                return
+        };
     }
 }
