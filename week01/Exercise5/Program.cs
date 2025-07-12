5b45897a927f5abb2e9146b0b319e72fb79ba5f1
littleRock0 <irochelle@byupathway.edu>
2025-07-12 00:24:20 -0600

    Excercise 5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -3,12 +3,54 @@
  * added using static System.Console; to shorten the code
 *******************************************************************************/
 using System;
+using System.ComponentModel;
 using static System.Console;
 
 class Program
 {
+    static void DisplayWelcome()
+    {
+        WriteLine("Welcome to the Program!");
+    }
+    
+    static string PromptUserName()
+    {
+        Write("User name: ");
+        
+        string name = ReadLine();
+        
+        return name;
+    }
+    
+    static int PromptUserNumber()
+    {
+        Write("Enter an Integer: ");
+        
+        int i = int.Parse(ReadLine());
+        
+        return i;
+    }
+    
+    static int SquareNumber(int i)
+    {
+        i *= i;
+        
+        return i;
+    }
+    
+    static void DisplayResult(string name, int i)
+    {
+        WriteLine($"{name}, your number squared = {i}");
+    }
+    
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+        
+        string userName = PromptUserName();
+        int yourNumber = PromptUserNumber();
+        int square = SquareNumber(yourNumber);
+        
+        DisplayResult(userName, square);
     }
 }
