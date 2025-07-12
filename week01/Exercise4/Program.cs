1647553c26b4e011335efbdc4a2030bc1639925e
littleRock0 <irochelle@byupathway.edu>
2025-07-11 23:11:58 -0600

    excersize 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -3,12 +3,43 @@
  * added using static System.Console; to shorten the code
  ******************************************************************************/
 using System;
+using System.Globalization; // don't know where this came from ?
 using static System.Console;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        WriteLine("Please enter thea series of numbers enter 0 when done");
+        
+        float input;
+        List<float> numbers = new List<float>();
+        float sum = 0;
+        float largest = 0;
+        
+        do
+        {
+            Write("Enter number: ");
+            input = float.Parse(ReadLine());
+            
+            if (input != 0)
+            {
+                numbers.Add(input);
+            }
+        } while (input != 0);
+        
+        foreach (float number in numbers)
+        {
+            sum += number;
+            
+            if (number > largest)
+            {
+                largest = number;
+            }
+        }
+        
+        float avg = sum / numbers.Count;
+        
+        WriteLine($"Sum: {sum}\nAverage: {avg}\nLargest number: {largest}");
     }
 }
