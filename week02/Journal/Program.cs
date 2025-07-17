8743dec529012ddb06cd739542ff865ef7d122e8
littleRock0 <irochelle@byupathway.edu>
2025-07-17 01:00:11 -0600

    moved implementation of menu items into separate functions and added switch statement to trigger them and fixed tabing

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,7 +1,12 @@
 /***************************************************************************
  * replaced WriteLine with Write to get rid of appended newline character
  * added using static System.Console; to shorten the code
- * I put the menu in a separate function
+ *
+ * Showing Creativity/Exceeding expectations
+ *   I put the menu in a separate function
+ *   also I put the implementation of each menu item in a separate function
+ *      and used a switch statement to call each function
+ *   I have 15 prompts instead of the required 5
 ***************************************************************************/
 using System;
 using static System.Console;
@@ -30,6 +35,62 @@ class Program
         return (options[--num], num);
     }
     
+    static string JWrite(Journal journal1)
+    {
+        PromptGenerator prompt = new PromptGenerator();
+        Entry newEntry = new Entry();
+        
+        newEntry._promptText = prompt.GetRandomPrompt();
+        
+        WriteLine(newEntry._promptText);
+        
+        newEntry._entryText = ReadLine();
+        
+        DateTime today = DateTime.Now.Date;
+        
+        newEntry._date = today.ToString("MM/dd/yyyy");
+        
+        journal1.AddEntry(newEntry);
+        
+        return "";
+    }
+    
+    static string JDisplay(Journal journal1)
+    {
+        journal1.DisplayAll();
+        
+        return "";
+    }
+    
+    static string Load(Journal journal1)
+    {
+        Write("File Name: ");
+        
+        string input = ReadLine();
+        
+        journal1.LoadFromFile(input);
+        
+        return "";
+    }
+    
+    static string Save(Journal journal1)
+    {
+        Write("File Name: ");
+        
+        string input = ReadLine();
+        
+        journal1.SaveToFile(input);
+        
+        return "";
+    }
+    
+    static string Quit(Journal journal1)
+    {
+        WriteLine("See you tomorrow!");
+        
+        return "";
+    }
+    
     static void Main(string[] args)
     {
         Journal journal1 = new Journal();
@@ -41,47 +102,25 @@ class Program
         {
             (selectName, selectNum) = Menu();
             
-            
             if (selectName == "Write")
             {
-                PromptGenerator prompt = new PromptGenerator();
-                Entry newEntry = new Entry();
-                
-                newEntry._promptText = prompt.GetRandomPrompt();
-                
-                WriteLine(newEntry._promptText);
-                
-                newEntry._entryText = ReadLine();
-                
-                DateTime today = DateTime.Now.Date;
-                
-                newEntry._date = today.ToString("MM/dd/yyyy");
-                
-                journal1.AddEntry(newEntry);
+                JWrite(journal1);
             }
             else if (selectName == "Display")
             {
-                journal1.DisplayAll();
+                JDisplay(journal1);
             }
             else if (selectName == "Load")
             {
-                Write("File Name: ");
-                
-                string input = ReadLine();
-                
-                journal1.LoadFromFile(input);
+                Load(journal1);
             }
             else if (selectName == "Save")
             {
-                Write("File Name: ");
-                
-                string input = ReadLine();
-                
-                journal1.SaveToFile(input);
+                Save(journal1);
             }
             else if (selectName == "Quit")
             {
-                WriteLine("See you tomorrow!");
+                Quit(journal1);
             }
         } while (selectNum != 5 && selectName != "Quit");
     }
