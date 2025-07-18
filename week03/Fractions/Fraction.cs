3edb4c661dd263fa6d90a5aeef478bfbffbbbc01
littleRock0 <irochelle@byupathway.edu>
2025-07-17 22:57:32 -0600

    basic structure

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,30 @@
+/***************************************************************************
+ * replaced WriteLine with Write to get rid of appended newline character
+ * added using static System.Console; to shorten the code
+***************************************************************************/
+using System;
+using static System.Console;
+
+public class Fraction
+{
+    private int _numerator;
+    private int _denominator;
+    
+    public Fraction()
+    {
+        _numerator = 1;
+        _denominator = 1;
+    }
+
+    public Fraction(int top)
+    {
+        _numerator = top;
+        _denominator = 1;
+    }
+    
+    public Fraction(int top, int bottom)
+    {
+        _numerator = top;
+        _denominator = bottom;
+    }
+}
--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,9 +1,22 @@
+/***************************************************************************
+ * replaced WriteLine with Write to get rid of appended newline character
+ * added using static System.Console; to shorten the code
+***************************************************************************/
 using System;
+using static System.Console;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        int num1 = 5;
+        int num2 = 3;
+        int num3 = 4;
+        int num4 = 1;
+
+        Fraction fraction1 = new Fraction();
+        Fraction fraction2 = new Fraction(num1);
+        Fraction fraction3 = new Fraction(num2, num3);
+        Fraction fraction4 = new Fraction(num4, num2);
     }
 }
