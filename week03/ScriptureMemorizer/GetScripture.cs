55c35ecd822ce08a114ffa1a211b6933c09366de
littleRock0 <irochelle@byupathway.edu>
2025-07-23 16:59:47 -0600

    added getscripture class and filled it out and integrated it

---
+++ week03/ScriptureMemorizer/GetScripture.cs
@@ -0,0 +1,40 @@
+/***************************************************************************
+ * replaced WriteLine with Write to get rid of appended newline character
+ * added using static System.Console; to shorten the code
+***************************************************************************/
+using System;
+using System.IO;
+using static System.Console;
+
+public class GetScripture()
+{
+    private List<string> _scriptures = new List<string>();
+    
+    public (string, int, int, int, string) GetRandomScripture()
+    {
+        string file = "Scriptures.txt";
+        string[] lines = File.ReadAllLines(file);
+        
+        foreach (string line in lines)
+        {
+            _scriptures.Add(line);
+        }
+        
+        Random number = new Random();
+        int scriptureNum = number.Next(0, lines.Length);
+        
+        string[] scriptureParts;
+        
+        string scripture = _scriptures[scriptureNum];
+        
+        scriptureParts = scripture.Split(",");
+        
+        string book = scriptureParts[0];
+        int chapter = int.Parse(scriptureParts[1]);
+        int verse = int.Parse(scriptureParts[2]);
+        int endVerse = int.Parse(scriptureParts[3]);
+        string text = scriptureParts[4];
+        
+        return (book, chapter, verse, endVerse, text);
+    }
+}
--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -1,6 +1,10 @@
 /***************************************************************************
  * replaced WriteLine with Write to get rid of appended newline character
  * added using static System.Console; to shorten the code
+ * more then three additional classes
+ * loaded scriptures from file
+ * gets a random scripture
+ * random selection only counts those that aren't already hidden
 ***************************************************************************/
 using System;
 using static System.Console;
@@ -9,14 +13,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        string verse = "Jesus wept.";
         string input;
         
-        Reference reference = new Reference("John", 11, 35);
-        Scripture scripture = new Scripture(reference, verse);
+        GetScripture aScripture = new GetScripture();
+        Scripture scripture;
+        Reference reference;
+        
+        (string book, int chapter, int verse, int endVerse, string text) =
+            aScripture.GetRandomScripture();
+        
+        if (endVerse == 0)
+        {
+            reference = new Reference(book, chapter, verse);
+            scripture = new Scripture(reference, text);
+        }
+        else
+        {
+            reference = new Reference(book, chapter, verse,
+                endVerse);
+            scripture = new Scripture(reference, text);
+        }
         
         do
         {
+            WriteLine("Press Enter to Continue or type quit to end.");
             WriteLine(scripture.GetDisplayText());
             
             input = ReadLine();
@@ -24,6 +44,8 @@ class Program
             if (scripture.IsCompletelyHidden())
             {
                 input = "quit";
+                WriteLine(reference.GetDisplayText() +
+                    " is completely hidden.");
             }
             
             if (input != "quit")
