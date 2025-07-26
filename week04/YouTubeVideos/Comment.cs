79a8a30f8b0b9cfc456a881081f0ba2f65d22a7b
littleRock0 <irochelle@byupathway.edu>
2025-07-26 16:09:14 -0600

    basic structure for YouTubeVideos

--- week04/YouTubeVideos/Comment.cs
+++ week04/YouTubeVideos/Comment.cs
@@ -7,5 +7,17 @@ using static System.Console;
 
 public class Comment
 {
+    private string _name;
+    private string _text;
     
+    public Comment(string name, string text)
+    {
+        _name = name;
+        _text = text;
+    }
+    
+    public string GetDisplayText()
+    {
+        
+    }
 }
--- week04/YouTubeVideos/Video.cs
+++ week04/YouTubeVideos/Video.cs
@@ -7,5 +7,34 @@ using static System.Console;
 
 public class Video
 {
+    private List<Comment> _comments = new List<Comment>();
+    private string _title;
+    private string _author;
+    private int _length;
     
+    public Video(string title, string author, int length)
+    {
+        _title = title;
+        _author = author;
+        _length = length;
+        
+        
+    }
+    
+    public int CommentCount()
+    {
+        int count = 0;
+        
+        foreach (Comment comment in _comments)
+        {
+            count++;
+        }
+        
+        return count;
+    }
+    
+    public string GetDisplayText()
+    {
+        
+    }
 }
