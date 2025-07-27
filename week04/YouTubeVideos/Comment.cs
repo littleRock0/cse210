0eb1edd02eb856ad96c5f4c00bb1d28b2270bcf6
littleRock0 <irochelle@byupathway.edu>
2025-07-26 23:58:26 -0600

    added place holder returns

--- week04/YouTubeVideos/Comment.cs
+++ week04/YouTubeVideos/Comment.cs
@@ -18,6 +18,6 @@ public class Comment
     
     public string GetDisplayText()
     {
-        
+        return "";
     }
 }
--- week04/YouTubeVideos/Video.cs
+++ week04/YouTubeVideos/Video.cs
@@ -35,6 +35,6 @@ public class Video
     
     public string GetDisplayText()
     {
-        
+        return "";
     }
 }
