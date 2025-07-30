4bc540562a3c2d251a14ee60e2b4d96a81c55530
littleRock0 <irochelle@byupathway.edu>
2025-07-30 14:37:29 -0600

    finished makeing the videos and comments

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -17,7 +17,7 @@ class Program
         video1.AddComment("AlfredoCheese", "Could you upload a detailed " +
             $"tutorial the Logan Utah Temple?");
         video1.AddComment("SoNotABot", "NiceVideo CouldWeGetSchems?");
-        
+
         Video video2 = new Video("Almost didn't make it back from this one",
             "climbing@redmont", 47);
         video2.AddComment("BLR", "Glad you made it back in one piece.");
@@ -25,16 +25,15 @@ class Program
         video2.AddComment("longtimefan", "Hope the wife doesn't see this " +
             "one ;P");
         video2.AddComment("#girls_climb2", "nice forearms");
-        
-        Video video3 = new Video("Minecraft LDS Temples Build", "RedMonster",
-            2192);
-        video3.AddComment("robot_pineapple", "Could you provide the " +
-            "material lists for each of these builds?");
-        video3.AddComment("robot_pineapple", "Could you provide the " +
-            "material lists for each of these builds?");
-        video3.AddComment("robot_pineapple", "Could you provide the " +
-            "material lists for each of these builds?");
-        video3.AddComment("robot_pineapple", "Could you provide the " +
-            "material lists for each of these builds?");
+
+        Video video3 = new Video("We Built A Giant Obstical Course",
+            "RedMonster", 2192);
+        video3.AddComment("littleRock", "I wish I had the money for this");
+        video3.AddComment("two2Rochelles", "Where did you source your " +
+            "materials?");
+        video3.AddComment("pineapple_crafts", "What was your budget for " +
+            "this project?");
+        video3.AddComment("total_whipe_out", "Could you drop us the plans" +
+            " for this ? ");
     }
 }
