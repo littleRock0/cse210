8670da3964495cac97cf104450be14b63e91bc39
littleRock0 <irochelle@byupathway.edu>
2025-07-30 19:51:28 -0600

    Fixed YouTube Video Display to interphase via a List instead of directly as before

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -7,6 +7,13 @@ using static System.Console;
 
 class Program
 {
+    static void Display(List<Video> videos)
+    {
+        foreach (Video video in videos)
+        {
+            Write(video.GetDisplayText());
+        }
+    }
     static void Main(string[] args)
     {
         Video video1 = new Video("Minecraft LDS Temples Build", "Kobayashi",
@@ -17,7 +24,7 @@ class Program
         video1.AddComment("AlfredoCheese", "Could you upload a detailed " +
             $"tutorial the Logan Utah Temple?");
         video1.AddComment("SoNotABot", "NiceVideo CouldWeGetSchems?");
-        
+
         Video video2 = new Video("Almost didn't make it back from this one",
             "climbing@redmont", 47);
         video2.AddComment("BLR", "Glad you made it back in one piece.");
@@ -25,7 +32,7 @@ class Program
         video2.AddComment("longtimefan", "Hope the wife doesn't see this " +
             "one ;P");
         video2.AddComment("#girls_climb2", "nice forearms");
-        
+
         Video video3 = new Video("We Built A Giant Obstical Course",
             "RedMonster", 2192);
         video3.AddComment("littleRock", "I wish I had the money for this");
@@ -35,9 +42,7 @@ class Program
             "this project?");
         video3.AddComment("total_whipe_out", "Could you drop us the plans" +
             " for this ? ");
-        
-        Write(video1.GetDisplayText());
-        Write(video2.GetDisplayText());
-        Write(video3.GetDisplayText());
+
+        Display(new List<Video> { video1, video2, video3 });
     }
 }
