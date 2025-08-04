a23f16b4a34622a3ad24d6b84cc14786374ee54c
littleRock0 <irochelle@byupathway.edu>
2025-08-04 15:05:51 -0600

    files added

---
+++ week05/Mindfulness/ReflectingActivity.cs
@@ -0,0 +1,6 @@
+/***************************************************************************
+ * replaced WriteLine with Write to get rid of appended newline character
+ * added using static System.Console; to shorten the code
+***************************************************************************/
+using System;
+using static System.Console;
