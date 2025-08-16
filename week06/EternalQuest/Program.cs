e7cfd39842b6c7a0d296ed976060d4156fd19d08
littleRock0 <irochelle@byupathway.edu>
2025-08-16 06:27:15 -0600

    added showing creativity comments

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -1,6 +1,12 @@
 /***************************************************************************
  * replaced WriteLine with Write to get rid of appended newline character
  * added using static System.Console; to shorten the code
+ * Showing Creativity:
+ *  added remove Goal function for if you want to remove completed goals or
+ *      a goal is nolonger viable
+ *  trying to stick as close as possible to the sample Class Design thing
+ *      resulted in a few very interesting solutions
+ * plan to add more if I have time but hopefully this is enough to qualify
 ***************************************************************************/
 using System;
 using static System.Console;
