99c5b799d7dfa05187dfaff361cd9929a116bc71
littleRock0 <irochelle@byupathway.edu>
2025-08-16 07:52:18 -0600

    tab fix

--- week06/EternalQuest/GoalManager.cs
+++ week06/EternalQuest/GoalManager.cs
@@ -100,7 +100,7 @@ public class GoalManager
             
             Write($"  {_goals.IndexOf(goal) + 1}. [{tick}] " +
                 $"{goal.GetDetailsString()}\n");
-
+            
             tick = ' ';
         }
     }
