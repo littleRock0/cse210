6fe155e2fee392639b15cc0f9db8d5a5eba54830
littleRock0 <irochelle@byupathway.edu>
2025-08-16 04:11:39 -0600

    base program done i think

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -9,6 +9,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the EternalQuest Project.");
+        GoalManager goalManager = new GoalManager();
+
+        goalManager.Start();
     }
 }
