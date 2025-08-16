d98876c421f25bb90ab1649116c3b3e755e0f4d8
littleRock0 <irochelle@byupathway.edu>
2025-08-16 16:22:39 -0600

    added a file

--- week07/ExerciseTracking/ExcersizeManager.cs
+++ week07/ExerciseTracking/ExcersizeManager.cs
@@ -8,4 +8,14 @@ using static System.Console;
 public class ExcersizeManager
 {
     private List<Excersize> _excersizes = new List<Excersize>();
+
+    public ExcersizeManager()
+    {
+        
+    }
+
+    public void Start()
+    {
+        Write("");
+    }
 }
